b98d76a90c8a6f90a1839026e63fcad78921b9eb
lzsllv <biscoil1xo@gmail.com>
2026-05-05 11:44:29 -0300

    feat: adiciona rota da atv-03

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Exemplo03 from './pages/exemplos/ex-03';
 
 import Atv01 from './pages/atividades/atv-01';
 import Atv02 from './pages/atividades/atv-02';
+import Atv03 from './pages/atividades/atv-03';
 import Atv04 from './pages/atividades/atv-04';
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
 
     <Route path="/atividade/1" element={<Atv01 />} />
     <Route path="/atividade/2" element={<Atv02 />} />
+    <Route path="/atividade/3" element={<Atv03 />} />
     <Route path="/atividade/4" element={<Atv04 />} />
 
   </Routes>
